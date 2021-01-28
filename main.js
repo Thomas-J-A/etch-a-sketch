84cfdc40ababceb6854b98ed4c6be8f40fa25bc1
Thomas <tomaspbury@icloud.com>
2021-01-28 17:47:16 +0000

    Add new grid button

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-let container = document.querySelector('.container');
+function createGrid(divCount) {
+    removeChildDivs();
+    for (let i = 0; i < divCount; i++) {
+        let div = document.createElement('div');
+        div.style.border = '1px solid black';
+        div.addEventListener('mouseover', addClass);
+        container.appendChild(div);
+    }
+}
+
+function removeChildDivs() {
+    const divs = Array.from(container.getElementsByTagName('div'));
+    if (divs) {
+        for (let i = 0; i < divs.length; i++) {
+        container.removeChild(divs[i]);
+    }
+    }
+}
 
 function addClass() {
     this.className = 'visited';
@@ -11,12 +28,24 @@ function clearScreen() {
     }
 }
 
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement('div');
-    div.style.border = '1px solid black';
-    div.addEventListener('mouseover', addClass);
-    container.appendChild(div);
+function createNewGrid() {
+    let size = +prompt('New size of grid: (max: 50)', '25');
+    if (size > 50) {
+        alert('Number greater than 50.');
+        return;
+    }
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    createGrid(size*size);
 }
 
-const btn = document.getElementById('clear');
-btn.addEventListener('click', clearScreen);
+
+let container = document.querySelector('.container');
+
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', clearScreen);
+
+const newGridBtn = document.getElementById('new-grid');
+newGridBtn.addEventListener('click', createNewGrid);
+
+createGrid(256);
